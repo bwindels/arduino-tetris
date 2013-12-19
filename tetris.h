0e4bb14286d2d3517638b196321768f0f43c2783
Bruno Windels <bruno.windels@gmail.com>
2013-12-19 07:44:10 +0100

    split up rotation

--- tetris.h
+++ tetris.h
@@ -1,7 +1,9 @@
 #include "types.h"
 
 //tetris logic, we use the framebuffer as the playing field to conserve memory
-bool tetris_rotate_piece(buffer_t* fb, buffer_t* piece, bool ccw);
+void tetris_rotate_square_buffer(buffer_t* src, buffer_t* dst, bool ccw);
+bool tetris_apply_rotated_piece(buffer_t* fb, buffer_t* piece, bool ccw);
+
 bool tetris_move_piece(buffer_t* fb, buffer_t* piece, position_t pos);
 dimension_t tetris_count_filled_lines(buffer_t* fb);
 void tetris_drop_lines(buffer_t* fb, dimension_t lineCount);
