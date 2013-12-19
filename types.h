4926d129ba02ca521cfe33ca723e04dc4ad6a4de
Bruno Windels <bruno.windels@gmail.com>
2013-12-19 07:30:11 +0100

    common types

---
+++ types.h
@@ -0,0 +1,11 @@
+typedef unsigned char pixel_t;
+typedef unsigned char dimension_t;
+typedef signed char direction_t;
+
+typedef struct {
+	direction_t x, y;
+} vector_t;
+
+typedef struct {
+	dimension_t x, y;
+} position_t;
